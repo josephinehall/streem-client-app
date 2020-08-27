ccac23f5d5f034bd9e0b91fa01688db3974a93e8
josephinehall <josephinehall@gmail.com>
2020-08-27 16:03:22 +1200

    Define a function to fetch data from the API
    Pass this as a prop to the Form component, so that the Form remains "dumb" and just calls the passed in function with the form data.

--- src/App.js
+++ src/App.js
@@ -8,9 +8,33 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { data: null };
+    this.fetchData = this.fetchData.bind(this);
+  fetchData(formData) {
+    var url = new URL(process.env.REACT_APP_API_URL);
+
+    var params = {
+      query: formData.get('query'),
+      after: formData.get('after'),
+      before: formData.get('before'),
+      interval: formData.get('interval'),
+    };
+
+    url.search = new URLSearchParams(params).toString();
+
+    fetch(url, {
+      headers: {
+        'Authorization': 'Token ' + process.env.REACT_APP_API_TOKEN
+      }
+    }).then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
+
   render() {
+    let data = this.state.data && this.state.data.aggregations.date_agg.buckets;
+
     return (
       <div className="App">
+        <Form fetchData={this.fetchData} />
         { (data != null) && <Graph data={this.formatData(data)} /> }
       </div>
     )
