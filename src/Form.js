e8d354b9eb18d29ae55bbd760ae6b312a123ddcb
josephinehall <josephinehall@gmail.com>
2020-08-27 16:06:11 +1200

    Reformat form markup to match Class definition

--- src/Form.js
+++ src/Form.js
@@ -12,51 +12,58 @@ class Form extends Component {
     this.props.fetchData(formData);
   }
 
-  return (
-      <div>
-        <label htmlFor="query">Query:</label>
-        <input
-          type="text"
-          id="query"
-          name="query"
-          required
-          placeholder="scott morrison"
-        />
-      </div>
-      <div>
-        <label htmlFor="after">Beginning at:</label>
-        <input
-          type="number"
-          id="after"
-          name="after"
-          required
-          placeholder="1554037199999"
-        />
-      </div>
-      <div>
-        <label htmlFor="before">Ending at:</label>
-        <input
-          type="number"
-          id="before"
-          name="before"
-          required
-          placeholder="1567209600000"
-        />
-      </div>
-      <div>
-        <label htmlFor="interval">Interval:</label>
-        <input
-          type="text"
-          id="interval"
-          name="interval"
-          required
-          placeholder="1d"
-        />
-      </div>
-      <button>Search</button>
-    </form>
-  );
+  render() {
+    return (
       <form onSubmit={this.handleSubmit}>
+        <div>
+          <label htmlFor="query">Query:</label>
+          <input
+            type="text"
+            id="query"
+            name="query"
+            required
+            placeholder="scott morrison"
+            value="scott morrison"
+          />
+        </div>
+        <div>
+          <label htmlFor="after">Beginning at:</label>
+          <input
+            type="number"
+            id="after"
+            name="after"
+            required
+            placeholder="1554037199999"
+            value="1554037199999"
+          />
+        </div>
+        <div>
+          <label htmlFor="before">Ending at:</label>
+          <input
+            type="number"
+            id="before"
+            name="before"
+            required
+            placeholder="1567209600000"
+            value="1567209600000"
+          />
+        </div>
+        <div>
+          <label htmlFor="interval">Interval:</label>
+          <input
+            type="text"
+            id="interval"
+            name="interval"
+            required
+            placeholder="1d"
+            value="10d"
+            onKeyUp={this.handleKey}
+          />
+        </div>
+        <button>Search</button>
+      </form>
+    )
+  }
 }
 
 export default Form;
