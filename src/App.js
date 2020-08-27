a3ef1bed9b48a4f314b6d064282dce76bcee17b0
josephinehall <josephinehall@gmail.com>
2020-08-27 16:00:39 +1200

    Render a Graph component based on the state of `data`

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import './App.css';
 import Form from './Form';
+import Graph from './Graph';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
+    this.state = { data: null };
+  render() {
+    return (
+      <div className="App">
+        { (data != null) && <Graph data={this.formatData(data)} /> }
+      </div>
+    )
   }
 }
 
